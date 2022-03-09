13c01658b4a13742f9e189fba559bd76fbc81b0a
BestC <chalongklongc@gmail.com>
2022-03-10 00:22:07 +0700

    image

--- lib/home/food_detail.dart
+++ lib/home/food_detail.dart
@@ -25,7 +25,7 @@ class FoodDetail extends StatelessWidget {
                   ),
                   onPressed: () => {Navigator.pop(context)},
                   icon: Icon(
-                    Icons.navigate_next,
+                    Icons.navigate_before,
                     color: Colors.white,
                   ),
                   label: Text(
